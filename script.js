68d3c158a671167ac0a336cb938ed377ee5ce9ff
elainedemarck <bortoluzzi.elaine@escola.pr.gov.br>
2026-06-17 11:18:15 -0300

    17

--- script.js
+++ script.js
@@ -14,7 +14,41 @@ for(let i =0; i<botoes.length;i++);{
 
 }
 constcontadores = document.querySelectorAll(".contador");
-consttempoObjetivo = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+consttempoObjetivo1 = new Date("2024-10-05T00:00:00");
+consttempoObjetivo2 = new Date("2024-12-05T00:00:00");
+consttempoObjetivo3 = new Date("2024-10-30T00:00:00");
+consttempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
-contadores[0].textContent= tempoObjetivo1- tempoAtual;
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo-tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math;floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+    if (tempoFinal>0){
+        return[dias,horas,minutos,segundos];
+    }else{
+  return[0,0,0,0];
+    }
+}
+
+function atualizaCronometro(){
+    for(let i=0; i<contadores.length; i++);{
+    document.getElementByld("dias"+i).textContent = calculatempo(tempos[i])[0];
+    document.getElementByld("horas"+i).textContent = calculatempo(tempos[i])[1];
+    document.getElementByld("min"+i).textContent = calculatempo(tempos[i])[2];
+    document.getElementByld("seg"+i).textContent = calculatempo(tempos[i])[3];
+}
+}
+function comecaCronometro();{
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
+}
+comecaCronometro
